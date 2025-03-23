8fdb63a60831d341b7e1c98f1283456b5d6a10db
佐藤瑞起 <117ema3cia@gmail.com>
2025-03-23 20:56:25 +0900

    include <cctype> 追加

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -6,11 +6,12 @@
 /*   By: mizusato <mizusato@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 03:51:25 by mizusato          #+#    #+#             */
-/*   Updated: 2025/03/23 20:34:32 by mizusato         ###   ########.fr       */
+/*   Updated: 2025/03/23 20:54:00 by mizusato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
 int	main(int argc, char **argv)
 {
@@ -20,9 +21,10 @@ int	main(int argc, char **argv)
 	{
 		for (int i = 1; i < argc; i++)
 		{
-			for (int j = 0; argv[i][j]; j++)
-				argv[i][j] = std::toupper(argv[i][j]);
-			std::cout << argv[i];
+			std::string str = argv[i];
+			for (int j = 0; str[j]; j++)
+				str[j] = std::toupper(str[j]);
+			std::cout << str;
 		}
 	}
 	std::cout << std::endl;

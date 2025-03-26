0da28e67f09cbe4075fd21b0bd02b5eb20fe9a42
佐藤瑞起 <117ema3cia@gmail.com>
2025-03-26 15:17:03 +0900

    途中経過

---
+++ CPP_Module_00/ex01/includes/PhoneBook.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mizusato <mizusato@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 01:39:32 by mizusato          #+#    #+#             */
+/*   Updated: 2025/03/26 01:40:52 by mizusato         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include "Contact.hpp"
+
+class PhoneBook
+{
+	private:
+
+	public:
+};
+
+#endif

06b84f965ec16ce4a121e51972d16af5d02cfc48
佐藤瑞起 <117ema3cia@gmail.com>
2025-04-27 01:21:31 +0900

    _displayTimestampまで

--- CPP_Module_00/ex02/srcs/Account.cpp
+++ CPP_Module_00/ex02/srcs/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: mizusato <mizusato@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 22:57:23 by mizusato          #+#    #+#             */
-/*   Updated: 2025/04/26 23:51:56 by mizusato         ###   ########.fr       */
+/*   Updated: 2025/04/27 01:19:56 by mizusato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,13 @@ Account::Account(int initial_deposit)
 // Destructor
 Account::~Account(void)
 {
+	_displayTimestamp();
 
+	std::cout
+	<< "index:" << _accountIndex << ";"
+	<< "amount:" << _amount << ";"
+	<< "closed" <<
+	std::endl;
 }
 
 // get the total number of the accounts
@@ -61,17 +67,41 @@ int	Account::getNbDeposits(void)
 	return (_totalNbDeposits);
 }
 
+// get the total number of withdrawals
 int	Account::getNbWithdrawals(void)
 {
 	return (_totalNbWithdrawals);
 }
 
+// display information of all accounts
 void	Account::displayAccountsInfos(void)
 {
+	_displayTimestamp();
 
+	std::cout
+	<< "accounts:" << getNbAccounts() << ";"
+	<< "total:" << getTotalAmount() << ";"
+	<< "deposits" << getNbDeposits() << ";"
+	<< "withdrawals" << getNbWithdrawals()
+	<< std::endl;
 }
 
+// display timestamp in the correct format
 void	Account::_displayTimestamp(void)
 {
+	time_t		now;
+	struct tm	time_now;
 
+	now = std::time(0);
+	time_now = *std::localtime(&now);
+
+	std::cout << "["
+	<< time_now.tm_year + 1900
+	<< std::setw(2) << std::setfill('0') << time_now.tm_mon + 1
+	<< std::setw(2) << std::setfill('0') << time_now.tm_mday
+	<< "_"
+	<< std::setw(2) << std::setfill('0') << time_now.tm_hour
+	<< std::setw(2) << std::setfill('0') << time_now.tm_min
+	<< std::setw(2) << std::setfill('0') << time_now.tm_sec
+	<< "] ";
 }

8a094ab977413dff211611023360edc8dfae31e8
佐藤瑞起 <117ema3cia@gmail.com>
2025-04-27 11:33:57 +0900

    Account.cpp add

--- CPP_Module_00/ex02/srcs/Account.cpp
+++ CPP_Module_00/ex02/srcs/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: mizusato <mizusato@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 22:57:23 by mizusato          #+#    #+#             */
-/*   Updated: 2025/04/27 01:19:56 by mizusato         ###   ########.fr       */
+/*   Updated: 2025/04/27 11:21:26 by mizusato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,60 +15,31 @@
 #include <iomanip>
 
 // initiate static values
-int	Account::_nbAccounts = 0;
-int	Account::_totalAmount = 0;
-int	Account::_totalNbDeposits = 0;
-int	Account::_totalNbWithdrawals = 0;
-
-// Constructor with initating deposit
-Account::Account(int initial_deposit)
-{
-	_accountIndex = _nbAccounts++;
-	_amount = initial_deposit;
-	_nbDeposits = 0;
-	_nbWithdrawals = 0;
-
-	_displayTimestamp();
-
-	std::cout
-	<< "index:" << _accountIndex << ";"
-	<< "amount:" << _amount << ";"
-	<< "created"
-	<< std::endl;
-}
-
-// Destructor
-Account::~Account(void)
-{
-	_displayTimestamp();
-
-	std::cout
-	<< "index:" << _accountIndex << ";"
-	<< "amount:" << _amount << ";"
-	<< "closed" <<
-	std::endl;
-}
+int		Account::_nbAccounts = 0;
+int		Account::_totalAmount = 0;
+int		Account::_totalNbDeposits = 0;
+int		Account::_totalNbWithdrawals = 0;
 
 // get the total number of the accounts
-int	Account::getNbAccounts(void)
+int		Account::getNbAccounts(void)
 {
 	return (_nbAccounts);
 }
 
 // get the total amount of the money of all accounts
-int	Account::getTotalAmount(void)
+int		Account::getTotalAmount(void)
 {
 	return (_totalAmount);
 }
 
 // get the total number of deposits
-int	Account::getNbDeposits(void)
+int		Account::getNbDeposits(void)
 {
 	return (_totalNbDeposits);
 }
 
 // get the total number of withdrawals
-int	Account::getNbWithdrawals(void)
+int		Account::getNbWithdrawals(void)
 {
 	return (_totalNbWithdrawals);
 }
@@ -83,6 +54,106 @@ void	Account::displayAccountsInfos(void)
 	<< "total:" << getTotalAmount() << ";"
 	<< "deposits" << getNbDeposits() << ";"
 	<< "withdrawals" << getNbWithdrawals()
+	<< std::endl;
+}
+
+// Constructor with initating deposit
+Account::Account(int initial_deposit)
+{
+	_accountIndex = _nbAccounts++;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_totalAmount += _amount;
+
+	_displayTimestamp();
+
+	std::cout
+	<< "index:" << _accountIndex << ";"
+	<< "amount:" << checkAmount() << ";"
+	<< "created"
+	<< std::endl;
+}
+
+// Destructor
+Account::~Account(void)
+{
+	_displayTimestamp();
+
+	std::cout
+	<< "index:" << _accountIndex << ";"
+	<< "amount:" << checkAmount() << ";"
+	<< "closed" <<
+	std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+
+	std::cout
+	<< "index:" << _accountIndex << ";"
+	<< "p_amount:" << checkAmount() << ";"
+	<< "deposit:" << deposit << ";";
+
+	_amount += deposit;
+	_nbDeposits++;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+
+	std::cout
+	<< "amount:" << checkAmount() << ";"
+	<< "nb_deposits:" << _nbDeposits
+	<< std::endl;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	_displayTimestamp();
+
+	std::cout
+	<< "index:" << _accountIndex << ";"
+	<< "p_amount:" << checkAmount() << ";";
+
+	if (checkAmount() < withdrawal)
+	{
+		std::cout
+		<< "withdrawal:refused"
+		<< std::endl;
+		return (false);
+	}
+	else
+	{
+		std::cout
+		<< "withdrawal:" << withdrawal << ";";
+
+		_amount -= withdrawal;
+		_nbWithdrawals++;
+		_totalAmount -= withdrawal;
+		_totalNbWithdrawals++;
+
+		std::cout
+		<< "amount:" << checkAmount() << ";"
+		<< "nb_withdrawals:" << _nbWithdrawals
+		<< std::endl;
+		return (true);
+	}
+}
+
+int		Account::checkAmount(void) const
+{
+	return (_amount);
+}
+
+void	Account::displayStatus(void) const
+{
+	_displayTimestamp();
+
+	std::cout
+	<< "index:" << _accountIndex << ";"
+	<< "amount:" << checkAmount() << ";"
+	<< "deposits:" << _nbDeposits << ";"
+	<< "withdrawals:" << _nbWithdrawals
 	<< std::endl;
 }
 

b9ca6819c8c253991313584b9f57c2efdb15b9c4
佐藤瑞起 <117ema3cia@gmail.com>
2025-10-20 01:15:16 +0900

    ヘッダー追加

--- CPP_Module_02/ex02/srcs/main.cpp
+++ CPP_Module_02/ex02/srcs/main.cpp
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mizusato <mizusato@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/20 01:13:58 by mizusato          #+#    #+#             */
+/*   Updated: 2025/10/20 01:13:59 by mizusato         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Fixed.hpp"
 
 int main( void )
 {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	
+
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;

34bbaec4748579eca5e3c0beba90554fc5229c96
佐藤瑞起 <117ema3cia@gmail.com>
2025-10-25 17:08:59 +0900

    feat: main関数のテスト追加

--- CPP_Module_04/ex00/srcs/main.cpp
+++ CPP_Module_04/ex00/srcs/main.cpp
@@ -1,9 +1,12 @@
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int	main()
 {
+	std::cout << "--- PDF Main Test (Polymorphism) ---" << std::endl;
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
@@ -18,5 +21,19 @@ int	main()
 	delete j;
 	delete i;
 
+	std::cout << std::endl;
+
+	std::cout << "--- Wrong Classes Test (No Polymorphism) ---" << std::endl;
+	const WrongAnimal* wrongMeta = new WrongAnimal();
+	const WrongAnimal* wrong_i = new WrongCat();
+
+	std::cout << wrong_i->getType() << " " << std::endl;
+	wrong_i->makeSound(); // WrongAnimalの音が出る
+	wrongMeta->makeSound();
+
+	delete wrongMeta;
+	delete wrong_i;
+
+	std::cout << std::endl;
 	return 0;
 }

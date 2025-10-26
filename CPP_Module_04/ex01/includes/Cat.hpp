8619f14937ac023378dabc333009838d6b32c04b
佐藤瑞起 <117ema3cia@gmail.com>
2025-10-26 17:22:47 +0900

    feat: Dog, Cat のヘッダーファイルに private なメンバを追加

--- CPP_Module_04/ex01/includes/Cat.hpp
+++ CPP_Module_04/ex01/includes/Cat.hpp
@@ -2,9 +2,13 @@
 # define CAT_HPP
 
 # include "Animal.hpp"
+# include "Brain.hpp"
 
 class Cat : public Animal
 {
+	private:
+		Brain*	_brain;
+
 	public:
 		Cat();
 		Cat(const Cat& other);
@@ -12,6 +16,7 @@ class Cat : public Animal
 		virtual ~Cat();
 
 		virtual void	makeSound() const;
+		Brain*			getBrain() const;
 };
 
 #endif

751302b6b5232300c8f9a2bab01d8f82ff87a1f2
佐藤瑞起 <117ema3cia@gmail.com>
2025-10-26 17:59:12 +0900

    feat: Cat.cpp にBrain属性を追加

--- CPP_Module_04/ex01/srcs/Cat.cpp
+++ CPP_Module_04/ex01/srcs/Cat.cpp
@@ -4,27 +4,39 @@ Cat::Cat()
 {
 	std::cout << "Cat: Default constructor called" << std::endl;
 	this->type = "Cat";
+	this->_brain = new Brain();
 }
 
 Cat::Cat(const Cat& other) : Animal(other)
 {
 	std::cout << "Cat: Copy constructor called" << std::endl;
+	this->_brain = new Brain(*other._brain);
 }
 
 Cat& Cat::operator=(const Cat& other)
 {
 	std::cout << "Cat: Copy assignment operator called" << std::endl;
 	if (this != &other)
+	{
 		Animal::operator=(other);
+		delete this->_brain;
+		this->_brain = new Brain(*other._brain);
+	}
 	return *this;
 }
 
 Cat::~Cat()
 {
 	std::cout << "Cat: Destructor called" << std::endl;
+	delete this->_brain;
 }
 
 void	Cat::makeSound() const
 {
 	std::cout << "Meow Meow!" << std::endl;
+}
+
+Brain*	Cat::getBrain() const
+{
+	return this->_brain;
 }

6f5e60940aeafd4abf271266a4e4c2be12a5d958
佐藤瑞起 <117ema3cia@gmail.com>
2025-10-29 02:07:25 +0900

    feat: Bureaucrat.hpp 追加

---
+++ CPP_Module_05/ex00/includes/Bureaucrat.hpp
@@ -0,0 +1,41 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <exception>
+
+class Bureaucrat
+{
+	private:
+		const std::string	name_;
+		int					grade_;
+
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator=(const Bureaucrat& other);
+		~Bureaucrat();
+
+		std::string	getName() const;
+		int			getGrade() const;
+
+		void		incrementGrade();
+		void		decrementGrade();
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif

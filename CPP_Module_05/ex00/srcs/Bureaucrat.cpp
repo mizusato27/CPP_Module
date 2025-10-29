952db15fb16f86228a9235c1223d67a7b324306d
佐藤瑞起 <117ema3cia@gmail.com>
2025-10-29 14:59:21 +0900

    feat: Bureaucrat.cpp 追加

--- CPP_Module_05/ex00/srcs/Bureaucrat.cpp
+++ CPP_Module_05/ex00/srcs/Bureaucrat.cpp
@@ -39,3 +39,33 @@ int	Bureaucrat::getGrade() const
 {
 	return this->_grade;
 }
+
+void	Bureaucrat::incrementGrade()
+{
+	if (_grade == HIGHEST_GRADE)
+		throw GradeTooHighException();
+	_grade--;
+}
+
+void	Bureaucrat::decrementGrade()
+{
+	if (_grade == LOWEST_GRADE)
+		throw GradeTooLowException();
+	_grade++;
+}
+
+const char*	Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "Error: Grade is too high!";
+}
+
+const char*	Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return "Error: Grade is too low!";
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat)
+{
+	os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
+	return os;
+}
